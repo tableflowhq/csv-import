b6418c055a4a5f141c1a8fb1fb37f86494ffbcb9
ciminelli <eric@inquery.io>
2023-01-30 17:29:20 -0800

    Updated remote URL

--- create.sh
+++ create.sh
@@ -3,7 +3,7 @@
 cd ~/ || (echo 'Failed to navigate to ~/' && exit)
 sudo yum install git || (echo 'Failed to install git ~/' && exit)
 sudo yum install golang || (echo 'Failed to install goland ~/' && exit)
-git clone https://github.com/ciminelli/pg_auto_trigger.git
+git clone https://github.com/PortolaLabs/pg_auto_trigger.git
 cd pg_auto_trigger || (echo 'Failed to install or navigate to ~/pg_auto_trigger' && exit)
 chmod +x deploy.sh
 cp -n env.example .env
