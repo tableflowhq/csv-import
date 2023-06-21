e41e8e95cda2ee7bfe331389995aa2a549cadefc
ciminelli <eric@inquery.io>
2023-06-21 11:53:27 -0700

    Updated API key confirmation copy

--- admin-ui/src/features/forms/ApiKey/ApiKeyConfirmation.tsx
+++ admin-ui/src/features/forms/ApiKey/ApiKeyConfirmation.tsx
@@ -11,7 +11,11 @@ export default function ApiKeyConfirmation({ onCancel, onConfirm = () => null }:
         </div>
         <div className={style.texts}>
           <h3>Regenerate API Key</h3>
-          <p>Are you sure you want to proceed? The action cannot be undone.</p>
+          <p>
+            Are you sure you want to regenerate your API key? Any applications using this key will no longer be able to access the API until they are
+            updated with the new key.
+          </p>
+          <p>This action cannot be undone.</p>
         </div>
       </div>
 
