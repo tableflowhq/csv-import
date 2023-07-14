c781fd944007d3c4cb5085cf2d3b7a38f03f1c40
ciminelli <eric@tableflow.com>
2023-07-14 15:49:00 -0700

    Removed JS SDK build directory

--- js-sdk/build/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { TableFlowImporterProps } from "./types/index";
-import "./index.css";
-export default function TableFlowImporter({ elementId, isOpen, onRequestClose, importerId, hostUrl, darkMode, primaryColor, metadata, closeOnClickOutside, className, ...props }: TableFlowImporterProps): string;
