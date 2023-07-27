560d92adeab8968e12587fad3a64236e9bea2516
Leandro Vieira <alotropico@gmail.com>
2023-07-27 03:03:16 -0300

    Add onComplete to React SDK

--- react-sdk/src/components/TableFlowImporter/types/index.ts
+++ react-sdk/src/components/TableFlowImporter/types/index.ts
@@ -9,4 +9,5 @@ export type TableFlowImporterProps = HTMLAttributes<HTMLDialogElement> & {
     primaryColor?: string;
     closeOnClickOutside?: boolean;
     metadata?: string;
+    onComplete?: (data: { data: any; error: any }) => void;
 };
