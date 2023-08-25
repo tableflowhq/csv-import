f700b6249211c326d319755293b91eec310d2154
Carlos Mercado <carlos.mercado.pc@gmail.com>
2023-08-25 16:31:51 -0500

    Skip header row selection setting and arrange

---
+++ admin-ui/src/features/settings/forms/SkipHeaderRowSelection/index.tsx
@@ -0,0 +1,39 @@
+import { ChangeEvent } from "react";
+import { useParams } from "react-router";
+import { Box, Errors, Switch } from "@tableflow/ui-library/build";
+import useGetImporter from "../../../../api/useGetImporter";
+import usePostImporter from "../../../../api/usePostImporter";
+import style from "./style/SkipHeaderRowSelection.module.scss";
+
+export default function SkipHeaderRowSelection() {
+  const { importerId } = useParams();
+
+  const { data: importer, isLoading } = useGetImporter(importerId && importerId !== "new" ? importerId : "");
+
+  const { skip_header_row_selection: enabled } = importer || {};
+
+  const { mutate, error } = usePostImporter("", importer?.id);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    mutate({ id: importer?.id, skip_header_row_selection: e?.target?.checked });
+  };
+
+  if (isLoading) return null;
+
+  return (
+    <>
+      <h3>Skip Header Row Selection</h3>
+      <p>
+        If enabled, the importer will skip the Header Row Selection step and always choose the first row of the file for the column headers. This
+        option can also be enabled in the SDK.
+      </p>
+      <Box className={style.box} variants={["bg-shade"]}>
+        <div className={style.switchWrapper}>
+          <label>Skip header row selection:</label>
+          <Switch className={style.switch} checked={!!enabled} onChange={onChange} />
+        </div>
+      </Box>
+      <Errors error={error} />
+    </>
+  );
+}
--- admin-ui/src/features/settings/index.tsx
+++ admin-ui/src/features/settings/index.tsx
@@ -2,14 +2,23 @@ import { Box } from "@tableflow/ui-library";
 import { Importer } from "../../api/types";
 import useComponentsStore from "../../stores/componentsStore";
 import Domains from "./forms/Domains";
+import SkipHeaderRowSelection from "./forms/SkipHeaderRowSelection";
 import style from "./style/Settings.module.scss";
+import React from "react";
 
 export default function Settings({ importer }: { importer: Importer }) {
   const components = useComponentsStore((state) => state.components);
   const { importerSettings: SettingsComponents } = components;
+  const renderedComponent = SettingsComponents ? <SettingsComponents /> : null;
+  const shouldRenderColumn = renderedComponent && React.Children.count(renderedComponent.props.children) > 0;
 
   return (
     <div className={style.container}>
+      {shouldRenderColumn && (
+        <div className={style.column}>
+          {renderedComponent}
+        </div>
+      )}
       <div className={style.column}>
         <h3>Allowed Domains</h3>
         <p>Add domains to restrict where the importer can be hosted. If no domains are added, the importer will allow uploads from anywhere.</p>
@@ -19,11 +28,9 @@ export default function Settings({ importer }: { importer: Importer }) {
         </Box>
       </div>
 
-      {SettingsComponents && (
-        <div className={style.column}>
-          <SettingsComponents />
-        </div>
-      )}
+      <div className={style.column}>
+        <SkipHeaderRowSelection />
+      </div>
     </div>
   );
 }
