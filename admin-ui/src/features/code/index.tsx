11d951f9cb3d56e12e8d79e71eec1bc3add4761c
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-09-07 18:33:06 +0530

    Added code syntax highlighting using react-syntax-highlighter

--- admin-ui/src/features/code/index.tsx
+++ admin-ui/src/features/code/index.tsx
@@ -1,13 +1,17 @@
 import { useMemo } from "react";
-import { Button, Input, useLocalStorage } from "@tableflow/ui-library";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { Button, Input, useLocalStorage, useThemeStore } from "@tableflow/ui-library";
 import notification from "../../utils/notification";
 import getCodeJavaScript from "./utils/getCodeJavaScript";
 import getCodeReact from "./utils/getCodeReact";
 import { CodeProps } from "./types";
 import style from "./style/Code.module.scss";
+import { colorBrewer as syntaxHighlighterStyle } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 export default function Code(props: CodeProps) {
   const [framework, setFramework] = useLocalStorage("framework", "react");
+
+  const theme = useThemeStore((state) => state.theme);
 
   const options = {
     React: { value: "react" },
@@ -19,6 +23,24 @@ export default function Code(props: CodeProps) {
   const copyToClipboard = (text: string) => {
     navigator.clipboard.writeText(text);
     notification({ type: "success", message: "Copied to clipboard" });
+  };
+
+  const customStyles = {
+    resize: "none" as "none",
+    display: "block",
+    width: "100%",
+    minHeight: 470,
+    margin: 0,
+    padding: 24,
+    border: theme === "dark" ? "1px solid #344054" : "1px solid #d0d5dd",
+    borderBottomRightRadius: 8,
+    borderBottomLeftRadius: 8,
+    fontFamily: "monospace",
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+    overflow: "hidden",
+    backgroundColor: theme === "dark" ? "#181A1F" : "#fff",
+    color: theme === "dark" ? "#fff" : "#000",
   };
 
   return (
@@ -39,7 +61,9 @@ export default function Code(props: CodeProps) {
         </Button>
       </div>
 
-      <textarea value={code} readOnly />
+      <SyntaxHighlighter language={framework === "react" ? "jsx" : "javascript"} style={syntaxHighlighterStyle} customStyle={customStyles}>
+        {code}
+      </SyntaxHighlighter>
     </div>
   );
 }
