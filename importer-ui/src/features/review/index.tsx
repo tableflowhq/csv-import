e8e91ade68a24cc2948c41a80573d8d692333846
Carlos Mercado <carlos.mercado.pc@gmail.com>
2023-09-26 16:21:47 -0500

    Fixed Filters options'

--- importer-ui/src/features/review/index.tsx
+++ importer-ui/src/features/review/index.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { Button, Errors, ToggleFilter, useThemeStore } from "@tableflow/ui-library";
+import { Option } from "@tableflow/ui-library/build/ToggleFilter/types";
 import { QueryFilter } from "../../api/types";
 import useReview from "../../api/useReview";
 import useSubmitReview from "../../api/useSubmitReview";
@@ -9,16 +10,16 @@ import { ReviewProps } from "./types";
 import style from "./style/Review.module.scss";
 import Complete from "../complete";
 
-const defaultOptions = [
-  { label: "All (0)", selected: false },
-  { label: "Valid (0)", selected: false },
-  { label: "Error (0)", selected: false, color: "#f04339" },
+const defaultOptions: Option[] = [
+  { label: "All (0)", filterValue: "all", selected: false },
+  { label: "Valid (0)", filterValue: "valid", selected: false },
+  { label: "Error (0)", filterValue: "error", selected: false, color: "#f04339" },
 ];
 
 export default function Review({ onCancel, onComplete, upload, template, reload, close }: ReviewProps) {
   const uploadId = upload?.id;
-  const [filter, setFilter] = useState<QueryFilter>("all"); // default value
-  const [filterOptions, setFilterOptions] = useState(defaultOptions);
+  const [filter, setFilter] = useState<QueryFilter>("all");
+  const [filterOptions, setFilterOptions] = useState<Option[]>(defaultOptions);
   const [isSubmitCompleted, setIsSubmitCompleted] = useState(false);
   const { data, isLoading }: any = useReview(uploadId, {
     staleTime: 0,
@@ -42,7 +43,7 @@ export default function Review({ onCancel, onComplete, upload, template, reload,
     all.label = `All ${data?.num_rows}`;
     valid.label = `Valid ${data?.num_valid_rows}`;
     error.label = `Error ${data?.num_error_rows}`;
-    const [selectedFilter] = defaultOptions.filter((option) => option.label.toLowerCase().includes(filter));
+    const [selectedFilter] = defaultOptions.filter((option) => option.filterValue === filter);
     if (selectedFilter) selectedFilter.selected = true;
     setFilterOptions([...filterOptions]);
     if (data?.is_stored) {
@@ -51,9 +52,7 @@ export default function Review({ onCancel, onComplete, upload, template, reload,
   }, [JSON.stringify(data)]);
 
   const onFilterChange = useCallback((option: string) => {
-    // TODO: this regex might not be necessary if we use a separate property for values in the filter component
-    const match = option.match(/^(\w+)(?=\s\d+)/);
-    match && setFilter(match[1].toLowerCase() as QueryFilter);
+    setFilter(option as QueryFilter);
   }, []);
 
   const handleSubmitClick = (e: React.MouseEvent<HTMLButtonElement>) => {
