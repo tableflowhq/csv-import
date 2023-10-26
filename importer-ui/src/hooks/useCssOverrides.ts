1bb274cca71cbda24e5b8b3b59c02b66aeb29d19
ciminelli <eric@tableflow.com>
2023-10-26 16:48:34 -0700

    Added error handling to css override parsing

--- importer-ui/src/hooks/useCssOverrides.ts
+++ importer-ui/src/hooks/useCssOverrides.ts
@@ -6,15 +6,19 @@ export default function useCssOverrides(cssOverrides?: string, enabled?: boolean
     if (!enabled) {
       return;
     }
-    const parsedCss = parseCssOverrides(cssOverrides);
-    if (parsedCss) {
-      let style = document.getElementById("css-overrides");
-      if (!style) {
-        style = document.createElement("style");
-        style.setAttribute("id", "css-overrides");
-        document.head.append(style);
+    try {
+      const parsedCss = parseCssOverrides(cssOverrides);
+      if (parsedCss) {
+        let style = document.getElementById("css-overrides");
+        if (!style) {
+          style = document.createElement("style");
+          style.setAttribute("id", "css-overrides");
+          document.head.append(style);
+        }
+        style.textContent = decodeURIComponent(parsedCss);
       }
-      style.textContent = decodeURIComponent(parsedCss);
+    } catch (error) {
+      console.error("Failed to parse CSS overrides", error);
     }
   }, [cssOverrides, enabled]);
 }
