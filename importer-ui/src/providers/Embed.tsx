4df31b3929bb0aab384d1cfefefc4282ca256717
ciminelli <eric@tableflow.com>
2023-11-08 18:08:00 -0800

    Apply hover color based on darkened primary color if provided

--- importer-ui/src/providers/Embed.tsx
+++ importer-ui/src/providers/Embed.tsx
@@ -69,6 +69,38 @@ export default function Embed({ children }: EmbedProps) {
   const setTheme = useThemeStore((state) => state.setTheme);
   const { colorMode, toggleColorMode } = useColorMode();
 
+  // Check if a hex color is valid
+  const isValidColor = (color: string) => /^#([0-9A-F]{3}){1,2}$/i.test(color);
+
+  // Expand a three-character hex to six characters
+  const expandHex = (color: string) => {
+    if (color.length === 4) {
+      // #rgb => #rrggbb
+      color = "#" + color[1] + color[1] + color[2] + color[2] + color[3] + color[3];
+    }
+    return color;
+  };
+
+  // Darken a hex color by a certain percent
+  const darkenColor = (color: string, percent: number) => {
+    if (!isValidColor(color)) return color;
+
+    color = expandHex(color); // Ensure the color is in 6-character format
+
+    const num = parseInt(color.replace("#", ""), 16),
+      amt = Math.round(2.55 * percent),
+      R = (num >> 16) - amt,
+      B = ((num >> 8) & 0x00ff) - amt,
+      G = (num & 0x0000ff) - amt;
+
+    return (
+      "#" +
+      (0x1000000 + (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 + (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 + (G < 255 ? (G < 1 ? 0 : G) : 255))
+        .toString(16)
+        .slice(1)
+    );
+  };
+
   useEffect(() => {
     setTheme(darkMode ? "dark" : "light");
     if (darkMode && colorMode === "light") toggleColorMode();
@@ -77,9 +109,10 @@ export default function Embed({ children }: EmbedProps) {
 
   // Apply primary color
   useEffect(() => {
-    if (primaryColor) {
+    if (primaryColor && isValidColor(primaryColor)) {
       const root = document.documentElement;
       root.style.setProperty("--color-primary", primaryColor);
+      root.style.setProperty("--color-primary-hover", darkenColor(primaryColor, 20));
     }
   }, [primaryColor]);
 
