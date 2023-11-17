c2fc59b99d9ef3db832056d455a036a9ad8e7a25
Carlos Mercado <carlos.mercado.pc@gmail.com>
2023-11-17 14:44:06 -0500

    Working progress admin-ui sidebar

--- admin-ui/src/features/layouts/main/index.tsx
+++ admin-ui/src/features/layouts/main/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import classes from "../../../utils/classes";
 import TopBar from "../../top-bar";
 import style from "./style/Main.module.scss";
+import SimpleSidebar from "../../sidebar";
 
 export default function Main({ center, children }: React.PropsWithChildren<{ center?: boolean }>) {
   return (
     <div className={style.container} data-layout="main">
-      <TopBar />
-      <div className={classes([style.content, center && style.center])}>{children}</div>
+      {/* <TopBar /> */}
+      <SimpleSidebar />
+      {/* <div className={classes([style.content, center && style.center])}>{children}</div> */}
     </div>
   );
 }
+
+
---
+++ admin-ui/src/features/sidebar/index.tsx
@@ -0,0 +1,133 @@
+import { ReactText } from "react";
+import { IconType } from "react-icons";
+import {
+  Box,
+  BoxProps,
+  CloseButton,
+  Drawer,
+  DrawerContent,
+  Flex,
+  FlexProps,
+  Icon,
+  IconButton,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+import Tableflow from "../../components/Tableflow";
+import { FiCompass, FiHome, FiMenu, FiSettings, FiStar, FiTrendingUp } from "react-icons/fi";
+
+interface LinkItemProps {
+  name: string;
+  icon: IconType;
+  url: string;
+}
+const LinkItems: Array<LinkItemProps> = [
+  { name: "Getting started", icon: FiHome, url: '/' },
+  { name: "Importers", icon: FiTrendingUp, url: '/importers' },
+  { name: "Data", icon: FiCompass, url: '/data' },
+  { name: "Settings", icon: FiSettings, url: '/settings' },
+];
+
+export default function SimpleSidebar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+      <SidebarContent onClose={() => onClose} display={{ base: "none", md: "block" }} />
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose} returnFocusOnClose={false} onOverlayClick={onClose} size="full">
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      {/* mobilenav */}
+      <MobileNav display={{ base: "flex", md: "none" }} onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {/* Content */}
+      </Box>
+    </Box>
+  );
+}
+
+interface SidebarProps extends BoxProps {
+  onClose: () => void;
+}
+
+const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+  return (
+    <Box
+      bg={useColorModeValue("white", "gray.900")}
+      borderRight="1px"
+      borderRightColor={useColorModeValue("gray.200", "gray.700")}
+      //   w={{ base: 'full', md: 60 }}
+      pos="fixed"
+      h="full"
+      {...rest}>
+      <Flex h="10" alignItems="center" mx="4">
+        <Tableflow color />
+        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
+      </Flex>
+      {LinkItems.map((link) => (
+        <NavItem key={link.name} icon={link.icon} link={link.url}>
+          {link.name}
+        </NavItem>
+      ))}
+    </Box>
+  );
+};
+
+interface NavItemProps extends FlexProps {
+  icon: IconType;
+  children: ReactText;
+  link: string;
+}
+const NavItem = ({ icon, link, children, ...rest }: NavItemProps) => {
+  return (
+    <Box as="a" href={link} style={{ textDecoration: "none" }} _focus={{ boxShadow: "none" }}>
+      <Flex
+        align="center"
+        p="2"
+        mx="3"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: "var(--color-secondary-hover)",
+          color: "white",
+        }}
+        {...rest}>
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: "white",
+            }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </Box>
+  );
+};
+
+interface MobileProps extends FlexProps {
+  onOpen: () => void;
+}
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+  return (
+    <Flex
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 24 }}
+      height="20"
+      alignItems="center"
+      bg={useColorModeValue("white", "gray.900")}
+      borderBottomWidth="1px"
+      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+      justifyContent="flex-start"
+      {...rest}>
+      <IconButton variant="outline" onClick={onOpen} aria-label="open menu" icon={<FiMenu />} />
+
+      <Tableflow color />
+    </Flex>
+  );
+};
