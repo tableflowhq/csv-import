c129de4742df14f7ea28b65d5c5fe6ff44c6bbbc
ciminelli <eric@tableflow.com>
2023-12-04 16:17:51 -0800

    Removed admin-ui directory

--- admin-ui/src/utils/typedSxMap.ts
+++
@@ -1,9 +0,0 @@
-import { SystemProps, SystemStyleObject } from "@chakra-ui/react";
-
-type ThemeSxProps = SystemStyleObject & SystemProps;
-
-type SxMap<ClassKey extends string> = Record<ClassKey, ThemeSxProps>;
-
-export const typedSxMap = <ClassKey extends string>(sxMap: SxMap<ClassKey>) => {
-  return sxMap;
-};
