4bb83a7812dfc3a49971a299a8734874b61820d0
Carlos Mercado <carlos@plentycode.com>
2024-01-16 15:46:53 -0500

    fixed js importer modal

--- src/js.tsx
+++ src/js.tsx
@@ -1,12 +1,23 @@
+import { createRef } from "react";
 import ReactDOM from "react-dom";
 import CSVImporter from "./components/CSVImporter";
 import { CSVImporterProps } from "./types";
 
-export { CSVImporter };
-
-type CreateImporterProps = CSVImporterProps & { domElement?: HTMLElement };
+type CreateImporterProps = CSVImporterProps & { domElement?: Element };
 
 export function createCSVImporter(props: CreateImporterProps) {
+  const ref = createRef<typeof CSVImporter & HTMLDialogElement>();
   const domElement = props.domElement || document.body;
-  return ReactDOM.render(<CSVImporter {...props} />, domElement);
+
+  ReactDOM.render(<CSVImporter ref={ref} {...props} />, domElement);
+
+  return {
+    instance: ref.current,
+    showModal: () => {
+      ref.current?.showModal?.();
+    },
+    closeModal: () => {
+      ref.current?.close?.();
+    },
+  };
 }
