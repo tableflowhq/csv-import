811244b26ea0a69e716f29a1fd907506fcea2248
Carlos Mercado <carlos.mercado.pc@gmail.com>
2024-01-26 19:18:01 -0500

    Moved base styles to chakra default styles to avoid overrides in the client app

--- src/importer/settings/chakra/styles.ts
+++ src/importer/settings/chakra/styles.ts
@@ -16,5 +16,67 @@ export const styles: Styles = {
     "*, *::before, &::after": {
       borderColor: "",
     },
+
+    "label, input, button, textarea, select": {
+      font: "inherit",
+      color: "inherit",
+      fontWeight: "inherit",
+      lineHeight: "inherit",
+      backgroundColor: "transparent",
+      border: "none",
+      textAlign: "inherit",
+      padding: 0,
+      fontSize: "inherit",
+      accentColor: "var(--color-primary)",
+    },
+    ".docs-story": {
+      backgroundColor: "var(--color-background)",
+    },
+    "#root": {
+      transition: "filter ease-out var(--speed), transform ease-out var(--speed)",
+    },
+    ".modal [data-root]": {
+      filter: "blur(var(--blurred))",
+      transform: "scale(0.99)",
+    },
+    "button:not(:disabled)": {
+      cursor: "pointer",
+    },
+    "p:not(:last-child)": {
+      marginBottom: "var(--m-s)",
+    },
+    a: {
+      font: "inherit",
+      color: "inherit",
+      cursor: "pointer",
+      _hover: {
+        textDecoration: "none",
+      },
+    },
+    strong: {
+      fontWeight: "bold",
+    },
+    em: {
+      fontStyle: "italic",
+    },
+    h1: {
+      fontSize: "var(--font-size-h)",
+      fontWeight: "700",
+    },
+    h2: {
+      fontSize: "var(--font-size-xxl)",
+      fontWeight: "500",
+    },
+    small: {
+      fontSize: "var(--font-size-s)",
+    },
+    hr: {
+      border: "1px solid var(--color-border)",
+      borderWidth: "0 0 1px 0",
+      margin: "var(--m-s) 0",
+    },
+    "svg.react-icon": {
+      display: "block",
+    },
   },
 };
